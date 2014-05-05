33f4c1fc29d24c7604c69807a8eb74b9a4dcaee3
gankkank <gankkank@gmail.com>
2014-05-05 12:21:07 +0800

    add patch for 2.1.1, ubuntu 14.04

--- recipes/default.sh
+++ recipes/default.sh
@@ -3,8 +3,10 @@ ubuntu*)
 pkg_essential="curl openssh-server man-db build-essential make"
 pkg_ubun12="libpcre3-dev libssl-dev libbz2-dev zlib1g-dev libcurl4-openssl-dev libssh2-1-dev libssh2-1"
 pkg="libyaml-dev libreadline-dev libffi-dev libncurses5-dev libgdbm-dev libdbm-deep-perl"
-#disable: win32ole X11 win
+#disable: win32ole X11 win tk
+#tk for gui
 #dbm gdbm
+${node[pkgtool]} update
 ${node[pkgtool]} install -y ${pkg_essential} ${pkg_ubun12} ${pkg}
 ;;
 centos*)
@@ -14,8 +16,23 @@ ${node[pkgtool]} install -y ${pkg_essential} ${pkg_cent6}
 ;;
 esac
 
-echo $compile_src_url
+#echo $compile_src_url
 
+case $compile_src_version in
+2.1.1)
+#use patch 2.1.1 for ubuntu 14.04
+compile from_url "$compile_src_url" patch "2.1.1"
+;;
+*)
 compile from_url "$compile_src_url"
+;;
+esac
 
+case $compile_src_prefix in
+/usr/local)
+echo "make sure /usr/local/{bin,sbin} in $PATH"
+;;
+*)
 set_basic_path
+;;
+esac
