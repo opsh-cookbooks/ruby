52cc974aed3817d97dcc875a5e0e4f31f54d5ab7
jimmy.yang <gankkank@gmail.com>
2014-05-14 20:10:45 +0800

    add sudo

--- recipes/desktop.sh
+++ recipes/desktop.sh
@@ -6,7 +6,13 @@ ubuntu*)
   pkg_essential="curl openssh-server man-db build-essential make"
   pkg_ubun12="libpcre3-dev libssl-dev libbz2-dev zlib1g-dev libcurl4-openssl-dev libssh2-1-dev libssh2-1"
   pkg="libyaml-dev libreadline-dev libffi-dev libncurses5-dev libgdbm-dev libdbm-deep-perl"
-  apt-install $pkg_essential $pkg_ubun12 $pkg 
+  if [ "$USER" == "root" ]
+  then
+    apt-get update &&  apt-get install $pkg_essential $pkg_ubun12 $pkg
+  else
+    sudo apt-get update
+    sudo apt-get install $pkg_essential $pkg_ubun12 $pkg 
+  if
 
   [ -d ~/.rbenv ] || git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
   [ -d ~/.rbenv/plugins/ruby-build ] || git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
