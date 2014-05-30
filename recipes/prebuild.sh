c58676656f94347732d5c0ec4dd0c74903614599
gankkank <gankkank@gmail.com>
2014-05-30 16:57:10 +0800

    use deb

--- recipes/prebuild.sh
+++ recipes/prebuild.sh
@@ -7,41 +7,16 @@ pkg="libyaml-dev libreadline-dev libffi-dev libncurses5-dev libgdbm-dev libdbm-d
 #tk for gui
 #dbm gdbm
 
-#${node[pkgtool]} update
-#${node[pkgtool]} install -y ${pkg_essential} ${pkg_ubun12} ${pkg}
 apt-install ${pkg_essential} ${pkg_ubun12} ${pkg}
 
 ;;
-centos*)
-pkg_essential="make gcc gcc-c++ git man"
-pkg_cent6="pcre pcre-devel zlib zlib-devel openssl-devel openssl"
-${node[pkgtool]} install -y ${pkg_essential} ${pkg_cent6}
+*)
+echo "not yet."
+exit 1
 ;;
 esac
 
-[ -f "/opt/ruby-2.1.2.tar.gz" ] || template_cp "ruby-2.1.2.tar.gz" /opt
-if [ ! -d "/opt/ruby-2.1.2" ]; then
-  cd /opt; tar zxvf ruby-2.1.2.tar.gz
-fi
 
-#echo $compile_src_url
+[ -f "/opt/ruby_2.1.2-1_ubuntu_amd64.deb" ] || template_cp "ruby_2.1.2-1_ubuntu_amd64.deb" /opt/
 
-#case $compile_src_version in
-#2.1.1)
-##use patch 2.1.1 for ubuntu 14.04
-#compile from_url "$compile_src_url" patch "2.1.1"
-#;;
-#*)
-#compile from_url "$compile_src_url"
-#;;
-#esac
-#
-#case $compile_src_prefix in
-#/usr/local)
-#echo "make sure /usr/local/{bin,sbin} in $PATH"
-#;;
-#*)
-#set_basic_path
-#;;
-#esac
-set_basic_path
+which ruby &> /dev/null || dpkg -i /opt/ruby_2.1.2-1_ubuntu_amd64.deb
