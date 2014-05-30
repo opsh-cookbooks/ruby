4f717ba0d2cc2868037f3ea847ca73daebc9cdf3
gankkank <gankkank@gmail.com>
2014-05-30 10:33:51 +0800

    add attribute for prebuild

---
+++ attributes/prebuild.sh
@@ -0,0 +1,6 @@
+compile_src_version="2.1.2"
+compile_src_path=/opt/srcv
+compile_src_prefix=/opt/ruby-${compile_src_version}
+#compile_src_configure=""
+#compile_src_configure="--disable-tk"
+#compile_src_install="make,make install"
